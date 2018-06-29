86d6b85d70559d2e0c96f3c9630fc1ad4146fe6e
Esepulveda18 <39999390+Esepulveda18@users.noreply.github.com>
2018-06-29 13:09:05 -0400

    Add files via upload

--- getJhipsterOnlineToken.py
+++ getJhipsterOnlineToken.py
@@ -4,9 +4,5 @@ import requests
 def JhipsterAuthenticate():
     url = "https://start.jhipster.tech/api/authenticate" 
     data = {"password":os.environ.get('JHIP_PASS'),"username":os.environ.get('JHIP_USERNAME'),"rememberMe":False}
-    print data
     token = requests.post(url,json=data)
-    print (token.text)
-
-JhipsterAuthenticate()
 
